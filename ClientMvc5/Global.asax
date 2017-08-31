2dd541a8b66a5a6442de72e3782bff5ff3e9c581
Onur KAPICI <onurkapici@gmail.com>
2017-08-31 23:04:38 +0300

    InitialCommit

---
+++ ClientMvc5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientMvc5.MvcApplication" Language="C#" %>
---
+++ StudentService/Student.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace StudentService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Student" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Student.svc or Student.svc.cs at the Solution Explorer and start debugging.
+    public class Student : IStudent
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
